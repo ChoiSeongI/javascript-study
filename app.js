98d9139cac06ce5685c6ef848d8045f0879718e7
choiseongi <ysceh0329@gamil.com>
2021-10-29 20:41:25 +0900

    message

---
+++ app.js
@@ -0,0 +1,14 @@
+const playerNmae = "bami";
+const playerPoints = 126126;
+const playerCute = true;
+const playerFat = "little bit";
+
+
+const player={
+    name:"bami",
+    points : 10,
+    cute:true,
+};
+console.log(player);
+console.log(player.name);
+player.name
